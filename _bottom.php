c5098ae40c8376588b7136a5942c96bb2e2dca45
dm-kolomiets <59975221+dm-kolomiets@users.noreply.github.com>
2023-01-16 22:40:28 +0200

    Create _bottom.php

---
+++ _bottom.php
@@ -0,0 +1,9 @@
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+
+<script defer src="js/jquery-3.6.0.min.js"></script>
+<script defer src="js/rellax.min.js"></script>
+<script defer src="js/gsap.min.js"></script>
+<script defer src="js/ScrollTrigger.min.js"></script>
+<script defer src="js/scroll-animate.js"></script>
+<script defer src="js/global.js"></script>
+<script defer src="js/SmoothScroll.js"></script>
