3b377008d72a2679adfafde655f221861c60b0dc
dm-kolomiets <59975221+dm-kolomiets@users.noreply.github.com>
2023-01-16 22:40:30 +0200

    Create _header.php

---
+++ _header.php
@@ -0,0 +1,52 @@
+<div class="header-inner">
+  <div class="layer-close"></div>
+  <div class="top-mobile-menu">
+    <div id="logo">
+      <a href="index.php">
+        <img src="img/logo.svg" alt="logo">
+        <img src="img/logo-dark.svg" alt="logo">
+      </a>
+    </div>
+    <div class="toggle-block">
+      <div class="nav-wrapp">
+        <nav>
+          <ul>
+            <li><a href="about.php">Link 1</a></li>
+            <li class="second-menu menu-item-has-children">
+              <a href="#">Link 2</a>
+              <i></i>
+              <ul class="sub-menu">
+                <li><a href="#">Link 2.1</a></li>
+                <li><a href="#">Link 2.2</a></li>
+                <li><a href="#">Link 2.3</a></li>
+                <li><a href="#">Link 2.4</a></li>
+                <li><a href="#">Link 2.5</a></li>
+              </ul>
+            </li>
+            <li><a href="franchising.php">Link 2</a></li>
+            <li><a href="where-to-buy.php">Link 3</a></li>
+            <li><a href="blog.php">Link 4</a></li>
+            <li><a href="contact.php">Link 5</a></li>
+          </ul>
+          <!-- show on mobiles -->
+          <ul class="social social-mobile custom_offset">
+            <li>
+              <a href="#" target="_blank">
+                <img src="img/social_1.svg" alt="">
+              </a>
+            </li>
+            <li>
+              <a href="#" target="_blank">
+                <img src="img/social_2.svg" alt="">
+              </a>
+            </li>
+          </ul>
+          <!-- show on mobiles -->
+        </nav>
+      </div>
+    </div>
+    <div class="header-right">
+      <div class="mobile-button"><span></span></div>
+    </div>
+  </div>
+</div>
